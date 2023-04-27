453420acb30d294bb919ba67ec350610ee39f61f
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2023-04-27 11:42:24 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
-import App from 'components/App';
-import { store } from "./redux/store";
-
+import  App  from 'components/App';
 import './index.css';
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
+   <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode> 
 );
